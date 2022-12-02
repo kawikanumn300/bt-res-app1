0377bd3f4a3550fa8979de9dbd4e1dd222fd49d8
teacher 01 <teacher.bt@betimes.biz>
2022-12-02 10:16:08 +0700

    editnavigator

--- android/app/src/screen/app.navigator.tsx
+++ android/app/src/screen/app.navigator.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { createNativeStackNavigator} from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import App from "../../../../App";
 import welcome from "../component/welcome.component/welcome.component";
 import Login from "../component/login.component/login";
-import Register from "../component/register.component/register";
+import Register from "../component/register.component/Register";
 
 
 
-const{ Navigator, Screen } = createNativeStackNavigator();
+const { Navigator, Screen } = createNativeStackNavigator();
 const AppNavigator = () => (
     <NavigationContainer>
         <Navigator initialRouteName="Welcome">
-            <Screen options={{headerShown:false}} name="Welcome" component={welcome}></Screen>
-            <Screen options={{headerShown:false}} name="Login" component={Login}></Screen>
-            <Screen options={{headerShown:false}} name="Register" component={Register}></Screen>
+            <Screen options={{ headerShown: false }} name="Welcome" component={welcome}></Screen>
+            <Screen options={{ headerShown: false }} name="Login" component={Login}></Screen>
+            <Screen options={{ headerShown: false }} name="Register" component={Register}></Screen>
         </Navigator>
     </NavigationContainer>
 )
