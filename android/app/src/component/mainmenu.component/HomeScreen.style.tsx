e781152f0484a621ecd5d19e1fdc4bc17f3e405c
teacher 01 <teacher.bt@betimes.biz>
2022-12-13 16:59:47 +0700

    yfyf

--- android/app/src/component/mainmenu.component/HomeScreen.style.tsx
+++ android/app/src/component/mainmenu.component/HomeScreen.style.tsx
@@ -2,20 +2,6 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingTop: 40,
-        backgroundColor: '#ecf0f1',
-      },
-      paragraph: {
-        margin: 24,
-        fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: '#34495e',
-      },
 
 })
 export { styles };
